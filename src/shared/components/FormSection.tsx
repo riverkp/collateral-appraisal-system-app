d0549f1f105d04b8f9baa33268353abe9a0c5525
riverkp <215100604+riverkp@users.noreply.github.com>
2025-07-08 16:35:00 +0700

    Add basic form section (can be used to map form data to component)

---
+++ src/shared/components/FormSection.tsx
@@ -0,0 +1,92 @@
+import { Controller, useFormContext } from 'react-hook-form';
+import TextInput from './TextInput';
+import type { HTMLAttributes } from 'react';
+import ListBox, { type ListBoxItem } from './ListBox';
+
+interface FormSectionProps<
+  Schema extends Record<string, any>,
+  Details extends { [K in keyof Schema]: FormField },
+> extends HTMLAttributes<HTMLDivElement> {
+  fields: Details;
+  namePrefix?: string;
+}
+
+export type FormField = TextInputField | ListBoxField;
+
+interface TextInputField extends BaseFormField {
+  type: 'text-input';
+  label: string;
+  defaultValue?: string;
+}
+
+interface ListBoxField extends BaseFormField {
+  type: 'listbox';
+  label: string;
+  options: ListBoxItem[];
+}
+
+interface BaseFormField {
+  key?: string;
+  className?: string;
+}
+
+const FormSection = <
+  Schema extends Record<string, any>,
+  Details extends { [K in keyof Schema]: FormField },
+>({
+  fields,
+  ...props
+}: FormSectionProps<Schema, Details>) => {
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext();
+
+  return (
+    <div className={props.className}>
+      {Object.entries(fields).map(([key, value]) => {
+        if (value.type === 'text-input') {
+          return (
+            <Controller
+              name={`${props.namePrefix}${key}`}
+              control={control}
+              defaultValue={value.defaultValue}
+              key={value.key ? value.key : key}
+              render={({ field }) => (
+                <TextInput
+                  label={value.label}
+                  error={getPath(errors, props.namePrefix + key)?.message?.toString()}
+                  className={value.className}
+                  {...field}
+                />
+              )}
+            />
+          );
+        } else if (value.type === 'listbox') {
+          return (
+            <Controller
+              name={`${props.namePrefix}${key}`}
+              control={control}
+              render={({ field }) => (
+                <ListBox
+                  options={value.options}
+                  label={value.label}
+                  className={value.className}
+                  {...field}
+                  value={field.value}
+                  onChange={field.onChange}
+                />
+              )}
+            />
+          );
+        }
+      })}
+    </div>
+  );
+};
+
+function getPath(obj: any, path: string): any {
+  return path.split('.').reduce((acc, key) => acc?.[key], obj);
+}
+
+export default FormSection;
