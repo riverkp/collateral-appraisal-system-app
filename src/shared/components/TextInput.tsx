3ba0da382a5d45f73c4ce3a50fc79da4a8d2b911
riverkp <215100604+riverkp@users.noreply.github.com>
2025-07-08 15:48:15 +0700

    Add TextInput

---
+++ src/shared/components/TextInput.tsx
@@ -0,0 +1,18 @@
+import { type InputHTMLAttributes } from 'react';
+import Input from './Input';
+
+interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  readonly?: boolean;
+  error?: string;
+}
+
+const TextInput = ({ label, error, readonly = false, ...props }: TextInputProps) => {
+  return (
+    <div className={props.className}>
+      <Input label={label} value={props.value} onChange={props.onChange} error={error} />
+    </div>
+  );
+};
+
+export default TextInput;
