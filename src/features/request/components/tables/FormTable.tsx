16c875a4a3f1cb3b7f1d7af827631d35b52a4f14
riverkp <215100604+riverkp@users.noreply.github.com>
2025-07-11 10:00:04 +0700

    Add error message and smoother interaction to form table

--- src/features/request/components/tables/FormTable.tsx
+++ src/features/request/components/tables/FormTable.tsx
@@ -1,7 +1,13 @@
 import Icon from '@/shared/components/Icon';
 import Input from '@/shared/components/Input';
 import { useState } from 'react';
-import { useFieldArray, useFormContext } from 'react-hook-form';
+import {
+  useController,
+  useFieldArray,
+  useFormContext,
+  type Control,
+  type FieldValues,
+} from 'react-hook-form';
 
 interface FormTableProps {
   name: string;
@@ -14,8 +20,17 @@ interface FormTableHeader {
   inputType?: string;
 }
 
+interface TableCellProps {
+  name: string;
+  index: number;
+  editIndex: number | undefined;
+  value: string;
+  header: FormTableHeader;
+  control: Control<FieldValues, any, FieldValues>;
+}
+
 const FormTable = ({ name, headers }: FormTableProps) => {
-  const { getValues, register, control } = useFormContext();
+  const { getValues, control } = useFormContext();
   const { append, remove } = useFieldArray({
     control,
     name: name,
@@ -25,6 +40,14 @@ const FormTable = ({ name, headers }: FormTableProps) => {
   const handleDeleteRow = (index: number) => {
     setEditIndex(undefined);
     remove(index);
+  };
+  const handleAddRow = () => {
+    const newRow: Record<string, any> = {};
+    for (const header of headers) {
+      newRow[header.name] = '';
+    }
+    append(newRow);
+    setEditIndex(getValues(name).length - 1);
   };
 
   return (
@@ -43,29 +66,28 @@ const FormTable = ({ name, headers }: FormTableProps) => {
             <tr key={field.id}>
               {headers.map(header => (
                 <td>
-                  {editIndex === index ? (
-                    <Input
-                      type={header.inputType}
-                      {...register(`${name}.${index}.${header.name}`)}
-                    />
-                  ) : (
-                    // @ts-ignore
-                    field[header.name]
-                  )}
+                  <TableCell
+                    name={name}
+                    index={index}
+                    editIndex={editIndex}
+                    value={field[header.name]}
+                    header={header}
+                    control={control}
+                  />
                 </td>
               ))}
               <td className="flex gap-2 justify-end">
                 {editIndex === index ? (
                   <button type="button" onClick={() => setEditIndex(undefined)}>
-                    <Icon style="solid" name="check" className='text-cyan-600' />
+                    <Icon style="solid" name="check" className="text-cyan-600" />
                   </button>
                 ) : (
                   <button type="button" onClick={() => setEditIndex(index)}>
-                    <Icon style="solid" name="pen" className='text-cyan-600' />
+                    <Icon style="solid" name="pen" className="text-cyan-600" />
                   </button>
                 )}
                 <button type="button" onClick={() => handleDeleteRow(index)}>
-                  <Icon style="solid" name="trash" className='text-red-500' />
+                  <Icon style="solid" name="trash" className="text-red-500" />
                 </button>
               </td>
             </tr>
@@ -74,17 +96,31 @@ const FormTable = ({ name, headers }: FormTableProps) => {
             <td colSpan={headers.length + 1} className="p-0">
               <button
                 type="button"
-                onClick={() => append({ name: '', contactNumber: '' })}
+                onClick={handleAddRow}
                 className="w-full flex items-center justify-center m-0 px-0 py-1 bg-neutral-100"
               >
                 <div className="bg-teal-600 size-6 rounded-full">
-                  <Icon style="solid" name="plus" className='text-neutral-100' />
+                  <Icon style="solid" name="plus" className="text-neutral-100" />
                 </div>
               </button>
             </td>
           </tr>
         </tbody>
       </table>
+    </div>
+  );
+};
+
+const TableCell = ({ name, index, editIndex, value, header, control }: TableCellProps) => {
+  const cellName = `${name}.${index}.${header.name}`;
+  const {
+    field,
+    fieldState: { error },
+  } = useController({ name: cellName, control });
+  return (
+    <div>
+      {editIndex === index ? <Input type={header.inputType} {...field} /> : <div>{value}</div>}
+      {error && <div className="mt-1 text-sm text-red-600">{error?.message}</div>}
     </div>
   );
 };
