ed6b254bd796ef5eb97fca34d782efc59b54566a
riverkp <215100604+riverkp@users.noreply.github.com>
2025-07-14 17:42:40 +0700

    Fix bg color in profile card

--- src/shared/components/ProfileCard.tsx
+++ src/shared/components/ProfileCard.tsx
@@ -9,7 +9,7 @@ interface ProfileCardProps extends HTMLAttributes<HTMLDivElement> {
 const ProfileCard = ({ avatar, label, description, ...props }: ProfileCardProps) => {
   return (
     <div className={`flex items-center gap-3 ${props.className}`}>
-      <div className="size-8 bg-gray-50">{avatar}</div>
+      <div className="size-8 bg-gray-50 rounded-full">{avatar}</div>
       <div className="flex flex-col gap-0">
         <div className="text-yellow-500 font-medium">{label}</div>
         <div className="text-xs text-zinc-600">{description}</div>
