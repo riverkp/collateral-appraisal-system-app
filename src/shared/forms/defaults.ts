5226bbe783a847826deac0ca8b67b42badbf6c94
riverkp <215100604+riverkp@users.noreply.github.com>
2025-07-14 17:43:00 +0700

    Format defaults and add title defaults

--- src/shared/forms/defaults.ts
+++ src/shared/forms/defaults.ts
@@ -1,56 +1,63 @@
-import type { CreateRequestRequestType } from "./v1";
+import { type CreateRequestRequestType, type RequestTitleDtoType } from './v1';
 
 export const createRequestRequestDefaults: CreateRequestRequestType = {
-  "purpose": "",
-  "hasAppraisalBook": false,
-  "priority": "Normal",
-  "reference": {
-    "prevAppraisalNo": "",
-    "prevAppraisalValue": 0,
-    "prevAppraisalDate": "2023-01-01T14:30:00Z"
+  purpose: '',
+  hasAppraisalBook: false,
+  priority: 'Normal',
+  reference: {
+    prevAppraisalNo: '',
+    prevAppraisalValue: 0,
+    prevAppraisalDate: '2023-01-01T14:30:00Z',
   },
-  "channel": "",
-  "occurConstInspec": 0,
-  "loanDetail": {
-    "loanApplicationNo": "",
-    "limitAmt": 0,
-    "totalSellingPrice": 0
+  channel: '',
+  occurConstInspec: 0,
+  loanDetail: {
+    loanApplicationNo: '',
+    limitAmt: 0,
+    totalSellingPrice: 0,
   },
-  "address": {
-    "houseNo": "",
-    "roomNo": "",
-    "floorNo": "",
-    "locationIdentifier": "",
-    "moo": "",
-    "soi": "",
-    "road": "",
-    "subDistrict": "",
-    "district": "",
-    "province": "",
-    "postcode": ""
+  address: {
+    houseNo: '',
+    roomNo: '',
+    floorNo: '',
+    locationIdentifier: '',
+    moo: '',
+    soi: '',
+    road: '',
+    subDistrict: '',
+    district: '',
+    province: '',
+    postcode: '',
   },
-  "contact": {
-    "contactPersonName": "",
-    "contactPersonContactNo": "",
-    "projectCode": ""
+  contact: {
+    contactPersonName: '',
+    contactPersonContactNo: '',
+    projectCode: '',
   },
-  "fee": {
-    "feeType": "",
-    "feeRemark": ""
+  fee: {
+    feeType: '',
+    feeRemark: '',
   },
-  "requestor": {
-    "requestorEmpId": "",
-    "requestorName": "",
-    "requestorEmail": "",
-    "requestorContactNo": "",
-    "requestorAo": "",
-    "requestorBranch": "",
-    "requestorBusinessUnit": "",
-    "requestorDepartment": "",
-    "requestorSection": "",
-    "requestorCostCenter": ""
+  requestor: {
+    requestorEmpId: '',
+    requestorName: '',
+    requestorEmail: '',
+    requestorContactNo: '',
+    requestorAo: '',
+    requestorBranch: '',
+    requestorBusinessUnit: '',
+    requestorDepartment: '',
+    requestorSection: '',
+    requestorCostCenter: '',
   },
-  "customers": [],
-  "properties": [],
-  "comments": []
+  customers: [],
+  properties: [],
+  comments: [],
+  titles: [],
+};
+
+export const requestTitleDtoDefaults: RequestTitleDtoType = {
+  collateral: {
+    collateralType: 'land',
+  },
 };
